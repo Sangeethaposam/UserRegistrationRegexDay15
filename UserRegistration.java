fb7fb0370159e4bb34b5d0955f8559c5d1ec51db
Sangeethaposam <120437076+Sangeethaposam@users.noreply.github.com>
2023-01-27 22:55:54 +0530

    Here i have the methods for validating firstname, lastname, email, mobilenumber and password.

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static boolean isValidUsername(String name)
+	public boolean isValidUsername(String name)
 	{
 		String regex = "^[A-Za-z]{3,29}$";
 		Pattern p = Pattern.compile(regex);
@@ -14,7 +14,7 @@ public class UserRegistration {
 		Matcher m = p.matcher(name);
 		return m.matches();
 	}
-	public static boolean isValidEmail(String email) {
+	public boolean isValidEmail(String email) {
 		String regex =  "^[a-zA-Z0-9][+_.-]+@[a-zA-Z0-9.-]+$";
 		Pattern p = Pattern.compile(regex);
 		if (email == null) {
@@ -23,7 +23,7 @@ public class UserRegistration {
 		Matcher m = p.matcher(email);
 		return m.matches();
 	}
-	public static boolean isValidMobileFormat(String mobileNumber) {
+	public boolean isValidMobileFormat(String mobileNumber) {
 		String regex =  "^(91 )([0-9]{10})$";
 		Pattern p = Pattern.compile(regex);
 		if (mobileNumber == null){
@@ -32,7 +32,7 @@ public class UserRegistration {
 		Matcher m = p.matcher(mobileNumber);
 		return m.matches();
 	}
-	public static boolean isValidPassword(String password) {
+	public boolean isValidPassword(String password) {
 		String regex =  "^([aA-zZ]{8})[0-9]{3}[@$+&*]{1}$";
 		Pattern p = Pattern.compile(regex);
 		if (password == null){
